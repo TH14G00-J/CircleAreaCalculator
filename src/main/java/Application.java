ff67aaee88c13efb10fe6e362a3e1ef8ad00057a
Thiago <tjose.corp@gmail.com>
2025-08-20 18:00:47 -0300

    Refactor project: migrate to Maven structure

---
+++ src/main/java/Application.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius;
+        char answer;
+        do {
+            System.out.print("Enter the radius of the circle: ");
+            radius = sc.nextDouble();
+            System.out.printf("Area = %.4f\n", Math.PI * radius * radius);
+            System.out.println("Do you want to calculate another area? [y/n]");
+            answer = sc.next().toUpperCase().charAt(0);
+        } while (answer == 'Y');
+        sc.close();
+        System.out.println("Program terminated.");
+    }
+}
